143e4ed818e0cf1c9ba3c817ab27641af85440a3
Fernando Ribas <fribas84@gmail.com>
2023-06-12 16:31:09 -0300

    error handling in form

--- src/pages/NewCustomer.jsx
+++ src/pages/NewCustomer.jsx
@@ -1,24 +1,55 @@
-import {useNavigate} from 'react-router-dom';
+import { useNavigate, Form, useActionData} from 'react-router-dom';
 import CustomerForm from '../components/CustomerForm';
+import Error from '../components/Error';
+
+export const action = async ({request})=>{
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    const errors = [];
+    if(Object.values(data).includes('')){
+        errors.push('All fields are mandatory!');
+    }
+
+    if(errors.length){
+        return errors;
+    }
+    return null;
+}
+
 export const NewCustomer = () => {
     const navigate = useNavigate();
-  return (
-    <>
-    <div className='font-black text-4xl text-blue-900'>New Customer</div>
-    <p className='mt-3'>Create new Customer:</p>
+    const errors = useActionData();
 
-    <div className='flex justify-end'>
-        <button 
-        className='bg-blue-800 text-white px-3 py-1 font-bold'
-        onClick={() => navigate(-1)}>
-            Back
-        </button>
-    </div>
+    console.log(errors);
+    return (
+        <>
+            <div className='font-black text-4xl text-blue-900'>New Customer</div>
+            <p className='mt-3'>Create new Customer:</p>
 
-    <div className='bg-white shadow rounded-sm md:w-3/4 mx-auto px-5 py-10 '>
-        <CustomerForm />
+            <div className='flex justify-end'>
+                <button
+                    className='bg-blue-800 text-white px-3 py-1 font-bold'
+                    onClick={() => navigate(-1)}>
+                    Back
+                </button>
+            </div>
 
-    </div>
-    </>
-  )
+            <div className='bg-white shadow rounded-sm md:w-3/4 mx-auto px-5 py-10 mt-20'>
+                <Form
+                    method='post'
+                >
+
+                    {errors?.length && errors.map( (error,i)=><Error key={i}>{error}</Error>)}
+                    <CustomerForm />
+                    <input
+                        type='submit'
+                        className='mt-5 w-full bg-blue-800 p-3  font-bold text-white text-lg'
+                        value='Save Customer'
+                    />
+                </Form>
+
+
+            </div>
+        </>
+    )
 }
