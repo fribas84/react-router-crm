d3dec42cb4b2eff3a2b42a30262df1849096a338
Fernando Ribas <fribas84@gmail.com>
2023-06-12 14:16:55 -0300

    customers table

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -3,9 +3,26 @@ const Customer = ({ customer }) => {
     const { name, company, email, phone, id } = customer;
 
     return (
-        <tr>
-            <td className='text-center'>{name}</td>
-            <td className='text-center'>{email}</td>
+        <tr className="border-b">
+            <td className='p-2 space-x-2'>
+                <p className="text-2xl text-gray-800">{name}</p>
+                <p>{company}</p>
+            </td>
+            <td className='p-2'>
+                <p className="text-gray-600"><span className="text-gray-800 font-bold">Email: </span>{email}</p>
+                <p className="text-gray-600"><span className="text-gray-800 font-bold">Phone: </span>{phone}</p>
+
+            </td>
+
+            <td className='p-2 flex gap-2'> 
+                <button type="button" className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs ">
+                    Edit
+                </button>
+                <button type="button" className="text-red-600 hover:text-red-700 uppercase font-bold text-xs">
+                   Delete 
+                </button>
+
+            </td>
 
         </tr>
     )
