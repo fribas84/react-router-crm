19d0cd6578205d331b54e676da66b0fb94364a0e
Fernando Ribas <fribas84@gmail.com>
2023-06-14 12:26:55 -0300

    using json-server to mimic a rest API

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,45 +1,10 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Customer from '../components/Customer';
+import { getCustomers } from '../api/customers';
 
 export const loader = ()=>{
-  const customers = [
-    {
-      "name": "John Doe",
-      "company": "Company 1",
-      "email": "mail@maiol.com",
-      "phone": "+112341234",
-      "id": 1
-    },
-    {
-      "name": "John Doe2",
-      "company": "Company 2",
-      "email": "mail@mail2.com",
-      "phone": "+1123412342",
-      "id": 2
-    },
-    {
-      "name": "John Doe3",
-      "company": "Company 3",
-      "email": "mail@mail3.com",
-      "phone": "+1123412343",
-      "id": 3
-    },
-    {
-      "name": "John Doe4",
-      "company": "Company 4",
-      "email": "mail@mail4.com",
-      "phone": "+1123412344",
-      "id": 4
-    },
-    {
-      "name": "John Doe 5",
-      "company": "Company 5",
-      "email": "mail@mail5.com",
-      "phone": "+1123412345",
-      "id": 5
-    }
- ]
+  const customers = getCustomers();
   return customers;
 }
 
